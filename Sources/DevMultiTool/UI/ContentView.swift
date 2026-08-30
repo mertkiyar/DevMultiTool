015a9ac8bfb8c5332fde4b6d316ca480924c536c
mertkiyar <mertkiyar69@gmail.com>
2026-08-30 14:57:56 +0300

    fix: add missing padding to tool detail view

--- Sources/DevMultiTool/UI/ContentView.swift
+++ Sources/DevMultiTool/UI/ContentView.swift
@@ -92,6 +92,7 @@ struct ContentView: View {
                         Divider()
                         
                         tool.view
+                            .padding()
                     }
                     .transition(.move(edge: .trailing))
                 }
